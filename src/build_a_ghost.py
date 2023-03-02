a5d44ce4c59b4c4132aaf2bb7361d50827794b27
MosElAgab <mostafa.elagab@gmail.com>
2023-03-02 15:29:02 +0000

    build_a_ghost_completed

--- src/build_a_ghost.py
+++ src/build_a_ghost.py
@@ -1,13 +1,19 @@
 
 
 class Ghost:
+    is_scared = False
+   
     def __init__(self, name, speed, colour):
         self.name = name
         self.speed = speed
         self.colour = colour
-        self.is_scared = False
+    
+    def __str__(self):
+        return f'This is {self.colour} {self.name}, watch out'
 
     def can_be_eaten(self):
+        if self.colour == 'blue':
+            self.is_scared == True
         if self.is_scared:
             return True
         else:
@@ -20,6 +26,34 @@ class Ghost:
     def speed_up(self):
         self.speed = round(self.speed * 1.1, 2)
 
+    
 
+class Blinky(Ghost):
+    def __init__(self, name, speed, colour):
+        super().__init__(name, speed, colour)
+        self.name = 'Blinky'
+        self.speed =  3
+        self.colour = 'red'
+
+class Pinky(Ghost):
+    def __init__(self, name, speed, colour):
+        super().__init__(name, speed, colour)
+        self.name = 'Pinky'
+        self.speed =  2
+        self.colour = 'pink'
+
+class Inky(Ghost):
+    def __init__(self, name, speed, colour):
+        super().__init__(name, speed, colour)
+        self.name = 'Inky'
+        self.speed =  4
+        self.colour = 'cyan'
+
+class Clyde(Ghost):
+    def __init__(self, name, speed, colour):
+        super().__init__(name, speed, colour)
+        self.name = 'Clyde'
+        self.speed =  1
+        self.colour = 'yellow'
     
 
--- test/test_build_a_ghost.py
+++ test/test_build_a_ghost.py
@@ -1,4 +1,8 @@
 from src.build_a_ghost import (Ghost)
+from src.build_a_ghost import (Blinky)
+from src.build_a_ghost import (Pinky)
+from src.build_a_ghost import (Inky)
+from src.build_a_ghost import (Clyde)
 
 def test_ghost_is_constructed_with_the_appropriate_properties():
     test_ghost = Ghost('Ducky', 3, 'yellow')
@@ -6,15 +10,15 @@ def test_ghost_is_constructed_with_the_appropriate_properties():
     assert test_ghost.speed == 3
     assert test_ghost.colour == 'yellow'
 
-def test_ghost_is_constructed_with_default_false_is_scared_property():
+def test_ghost_is_constructed_with_default_false_is_scared_class_attribute():
     test_ghost = Ghost('Ducky', 3, 'yellow')
     assert test_ghost.is_scared is False
     
 def test_ghost_can_be_eaten():
     test_ghost = Ghost('Ducky', 6, 'green')
     assert test_ghost.can_be_eaten() is False
-    test_ghost.is_scared = True
-    assert test_ghost.can_be_eaten() is True
+    # test_ghost.is_scared = True
+    # assert test_ghost.can_be_eaten() is True
 
 def test_ghost_is_frighten_method_changes_colour_and_is_scared_properties():
     ducky = Ghost('ducky', 10, 'green')
@@ -29,5 +33,62 @@ def test_ghost_speed_up_method_increases_speed_by_10_percent():
     ducky.speed_up()
     ducky.speed_up()
     assert ducky.speed == 13.31
+
+def test_Blinky_Ghost_inheritance_to_all_parrent_class_methods_and_properties():
+    test_ghost = Blinky('xx', 2, 'green')
+    assert not test_ghost.is_scared
+    assert test_ghost.name == 'Blinky'
+    assert test_ghost.speed == 3
+    assert test_ghost.colour == 'red'
+    test_ghost.speed_up()
+    assert test_ghost.speed == 3.3
+    assert str(test_ghost)== 'This is red Blinky, watch out'
+    test_ghost.frighten()
+    assert test_ghost.colour == 'blue'
+    assert test_ghost.is_scared
+    assert test_ghost.can_be_eaten()
+
+def test_Pinky_Ghost_inheritance_to_all_parrent_class_methods_and_properties():
+    test_ghost = Pinky('xx', 2, 'green')
+    assert not test_ghost.is_scared
+    assert test_ghost.name == 'Pinky'
+    assert test_ghost.speed == 2
+    assert test_ghost.colour == 'pink'
+    test_ghost.speed_up()
+    assert test_ghost.speed == 2.2
+    assert str(test_ghost)== 'This is pink Pinky, watch out'
+    test_ghost.frighten()
+    assert test_ghost.colour == 'blue'
+    assert test_ghost.is_scared
+    assert test_ghost.can_be_eaten()
+
+def test_Inky_Ghost_inheritance_to_all_parrent_class_methods_and_properties():
+    test_ghost = Inky('xx', 2, 'green')
+    assert not test_ghost.is_scared
+    assert test_ghost.name == 'Inky'
+    assert test_ghost.speed == 4
+    assert test_ghost.colour == 'cyan'
+    test_ghost.speed_up()
+    test_ghost.speed_up()
+    assert test_ghost.speed == 4.84
+    assert str(test_ghost)== 'This is cyan Inky, watch out'
+    test_ghost.frighten()
+    assert test_ghost.colour == 'blue'
+    assert test_ghost.is_scared
+    assert test_ghost.can_be_eaten()
+
+def test_Clyde_Ghost_inheritance_to_all_parrent_class_methods_and_properties():
+    test_ghost = Clyde('xx', 2, 'green')
+    assert not test_ghost.is_scared
+    assert test_ghost.name == 'Clyde'
+    assert test_ghost.speed == 1
+    assert test_ghost.colour == 'yellow'
+    test_ghost.speed_up()
+    assert test_ghost.speed == 1.1
+    assert str(test_ghost)== 'This is yellow Clyde, watch out'
+    test_ghost.frighten()
+    assert test_ghost.colour == 'blue'
+    assert test_ghost.is_scared
+    assert test_ghost.can_be_eaten()
 
     
